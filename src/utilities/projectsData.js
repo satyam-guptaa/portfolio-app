5a03dc8dfca41e1105530b76a0cb6b29c739b28c
satyam gupta <satyamgupta2997@gmail.com>
2023-07-24 13:45:38 +0530

    initial push projects section
    initial push projects section

---
+++ src/utilities/projectsData.js
@@ -0,0 +1,28 @@
+const projects = [
+	{
+		title: 'Entertainment Web App',
+		stack: ['HTML', 'CSS', 'JS'],
+		img: '/assets/images/thumbnail-project-1-large.webp',
+		placeholderImg: '/assets/images/placeholder-1.png',
+	},
+	{
+		title: 'Portfolio',
+		stack: ['HTML', 'CSS', 'JS'],
+		img: '/assets/images/thumbnail-project-2-large.webp',
+		placeholderImg: '/assets/images/placeholder-1.png',
+	},
+	{
+		title: 'TODO App',
+		stack: ['HTML', 'CSS', 'JS'],
+		img: '/assets/images/thumbnail-project-3-large.webp',
+		placeholderImg: '/assets/images/placeholder-1.png',
+	},
+	{
+		title: 'Dictionary App',
+		stack: ['HTML', 'CSS', 'JS'],
+		img: '/assets/images/thumbnail-project-4-large.webp',
+		placeholderImg: '/assets/images/placeholder-1.png',
+	},
+];
+
+export default projects;
